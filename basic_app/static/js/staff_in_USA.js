dd698258f593f2877a5f026f932850845e38d4bf
Hongfei Xie <daz657174868@gmail.com>
2018-09-25 19:22:15 -0700

    add create invoice

--- basic_app/static/js/staff_in_USA.js
+++ basic_app/static/js/staff_in_USA.js
@@ -11,3 +11,21 @@ function addRow() {
     newRow.appendChild(newTH2);
     parent.appendChild(newRow);
 }
+
+function addDetail(){
+    var parent = document.getElementById("detailbody");
+    var newRow = document.createElement("TR");
+    var newTH1 = document.createElement("TH");
+    var newTH2 = document.createElement("TH");
+    var newTH3 = document.createElement("TH");
+    var newInput1 = document.createElement("INPUT");
+    var newInput2 = document.createElement("INPUT");
+    var newInput3 = document.createElement("INPUT");
+    newTH1.appendChild(newInput1);
+    newTH2.appendChild(newInput2);
+    newTH3.appendChild(newInput3);
+    newRow.appendChild(newTH1);
+    newRow.appendChild(newTH2);
+    newRow.appendChild(newTH3);
+    parent.appendChild(newRow);
+}
